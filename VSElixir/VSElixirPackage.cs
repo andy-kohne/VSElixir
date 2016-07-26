eedd33c02a820b827340fede005d98c32de61a44
Kohne <akohne@kaiglan.com>
2016-07-26 13:31:48 -0400

    setup for nuget package build

--- VSElixir/VSElixirPackage.cs
+++ VSElixir/VSElixirPackage.cs
@@ -14,7 +14,7 @@ using VSElixir.Helpers;
 namespace VSElixir
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("VSElixir", "Miscellaneous additions to VS", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("VSElixir", "IIS auto-attaching, solution cleaning, etc...", "1.0.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
