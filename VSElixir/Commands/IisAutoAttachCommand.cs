090faac698c326874758e6109cff68641ec354b1
khanhdle <khanh.dle@outlook.com>
2019-04-12 09:39:06 -0400

    Add support for attaching to Windows Service and fix performance issue of IIS process attach.

--- VSElixir/Commands/IisAutoAttachCommand.cs
+++ VSElixir/Commands/IisAutoAttachCommand.cs
@@ -2,6 +2,7 @@
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
@@ -75,6 +76,15 @@ namespace VSElixir.Commands
                 pane.WriteLine($"Solution: {slnName}");
 
                 // find physical paths in IIS
+                pane.WriteLine("Looking for Windows Service paths...");
+                var winPaths = WindowsService.GetWorkerProcesses(pane);
+                var attachedList = new List<string>();
+                if (winPaths.Count > 0)
+                {
+                    WindowsServiceAttachResolver.AttachDebuggerToWindowsService(winPaths, ide, pane, attachedList);
+                }
+
+                // find physical paths in IIS
                 pane.WriteLine("Looking for virtual directory physical paths...");
                 var iisItems = Iis.FindIisPaths(pane);
                 if (iisItems == null) return;
@@ -112,7 +122,7 @@ namespace VSElixir.Commands
                 foreach (var wp in wps)
                     pane.WriteLine($"  {wp.AppPool} - {wp.ProcessId}");
 
-
+                var localProcesses = ide.Debugger.LocalProcesses.OfType<Process2>().ToList();
                 foreach (var appPoolName in projects.Select(p => p.AppPoolName).Distinct())
                 {
                     pane.WriteLine($"Looking for {appPoolName} worker processes, used by {projects.First(o => string.Equals(o.AppPoolName, appPoolName, StringComparison.OrdinalIgnoreCase)).ProjectName}");
@@ -126,15 +136,15 @@ namespace VSElixir.Commands
                     pane.WriteLine($"  Target worker process for {workerProcess.ProcessId}: {appPoolName}");
 
                     // look for the process thru the debugger
-                    var en = ide.Debugger.LocalProcesses.GetEnumerator();
+                    var en = localProcesses.GetEnumerator();
                     pane.WriteLine("  Searching for matching local process...");
                     while (en.MoveNext())
                     {
-                        if (((Process2)en.Current).ProcessID != workerProcess.ProcessId) continue;
+                        if (en.Current.ProcessID != workerProcess.ProcessId) continue;
                         try
                         {
                             pane.WriteLine("  Attaching debugger...");
-                            ((Process2)en.Current).Attach();
+                            en.Current.Attach();
                         }
                         catch (Exception ex)
                         {
@@ -156,5 +166,78 @@ namespace VSElixir.Commands
 
         }
 
+    }
+
+    static class WindowsServiceAttachResolver
+    {
+        public static void AttachDebuggerToWindowsService(List<WindowsProcessDetail> list, DTE2 ide, OutputWindowPane pane, List<string> attachedList)
+        {
+            try
+            {
+                if (!list.Any())
+                {
+                    pane.WriteLine("NONE FOUND!");
+                    return;
+                }
+
+                // find any projects sited in iis
+                pane.WriteLine("Looking for projects in Windows Service...");
+
+                var projects = ide.Solution.GetAllProjects()
+                            .Where(p => p.Kind != EnvDTE.Constants.vsProjectKindUnmodeled && !string.IsNullOrEmpty(p.Name))
+                            .Select(x => new
+                            {
+                                Name = x.Name,
+                                FullName = x.FullName
+                            })
+                            .ToList();
+
+                var needToAttachProcesses = new List<WindowsProcessDetail>();
+                foreach (var x in list)
+                {
+
+                    var project = projects.FirstOrDefault(p => p.Name.ToLower() == x.ProcessName.ToLower() && (Path.GetDirectoryName(p.FullName) + "\\bin\\Debug").ToLower() == Path.GetDirectoryName(x.Path).ToLower());
+                    if (project == null)
+                        continue;
+                    needToAttachProcesses.Add(x);
+                    attachedList.Add(x.ProcessName);
+                }
+
+                if (!needToAttachProcesses.Any())
+                {
+                    pane.WriteLine("NONE FOUND!");
+                    return;
+                }
+                foreach (var p in needToAttachProcesses)
+                    pane.WriteLine($"   {p.ProcessName} - {p.Path}");
+
+                // find all the Windows processes
+                // look for the process thru the debugger
+                var en = ide.Debugger.LocalProcesses.OfType<Process2>()?.GetEnumerator();
+                pane.WriteLine("  Searching for matching local process...");
+                while (en.MoveNext())
+                {
+                    var processId = en.Current.ProcessID;
+                    var targetProcess = needToAttachProcesses.FirstOrDefault(x => x.Id == processId);
+                    if (targetProcess == null) continue;
+                    pane.WriteLine($"  Target process for {targetProcess.ProcessName}");
+                    try
+                    {
+                        pane.WriteLine("  Attaching debugger...");
+                        en.Current.Attach();
+                    }
+                    catch (Exception ex)
+                    {
+                        pane.WriteLine($"  ERROR: {ex}");
+                    }
+                }
+
+                pane.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                pane?.OutputString("ERROR: " + ex);
+            }
+        }
     }
 }
---
+++ VSElixir/Helpers/WindowsService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using EnvDTE;
+using Microsoft.Web.Administration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text.RegularExpressions;
+using Process = System.Diagnostics.Process;
+
+namespace VSElixir.Helpers
+{
+    public class WindowsService
+    {
+        private static readonly string[] PROCESS_NAMES = { "ktbs.services.rpchost", "kaigapi.rpchost", "payrollapi.rpchost" };
+        private static readonly string[] EXCLUDED = {
+            "csrss", "conhost", "svchost", "msbuild", "chrome", "msedge", "dllhost", "cmd", "iisexpress", "winlogon", "system", "smss", "idle"
+        };
+        public static List<WindowsProcessDetail> GetWorkerProcesses(OutputWindowPane pane)
+        {
+            try
+            {
+                var list = new List<WindowsProcessDetail>();
+                var processes = Process.GetProcesses().Where(x => !EXCLUDED.Contains(x.ProcessName.ToLower())).ToList();//windows services has not window title
+                foreach (var p in processes)
+                {
+                    try
+                    {
+                        list.Add(new WindowsProcessDetail
+                        {
+                            Id = p.Id,
+                            ProcessName = p.ProcessName,
+                            Path = p.MainModule?.FileName ?? string.Empty
+                        });
+                    }
+                    catch (System.ComponentModel.Win32Exception) { }//ignore win32
+                    catch (System.InvalidOperationException) { }//ignore
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                pane.WriteLine("EXCEPTION WHILE TRYING TO FIND WINDOWS PROCESSES!\nIs IDE running as admin?\n");
+                return null;
+            }
+        }
+
+
+    }
+
+    public class WindowsProcessDetail
+    {
+        public int Id { get; set; }
+        public string ProcessName { get; set; }
+        public string Path { get; set; }
+    }
+}
